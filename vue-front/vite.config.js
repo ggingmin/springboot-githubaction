60fc373c27ff9cfe4665eeb781bf396868155ce6
KYEONGMIN CHO <ggingmin@gmail.com>
2022-09-04 01:14:14 +0900

    vue frontend integrated

---
+++ vue-front/vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: '../src/main/resources/static', // this line place index.html in the public folder
+    assetsDir: './dist', // this line place your assets in the public/dist folder
+  }
+})
